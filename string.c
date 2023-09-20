808beb5953dfd940c6c20dea7547ade19b88d0dd
kwamepaadebra <kwamepolley215@gmail.com>
2023-09-20 07:21:59 -0700

    updating

--- string.c
+++ string.c
@@ -8,15 +8,14 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int i = 0;
 
-    if (!s)
-        return (0);
+	if (!s)
+		return (0);
 
-    while (*s++)
-        length++;
-
-    return (length);
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
@@ -28,18 +27,17 @@ int _strlen(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && *s2)
-    {
-        if (*s1 != *s2)
-            return (*s1 - *s2);
-        s1++;
-        s2++;
-    }
-
-    if (*s1 == *s2)
-        return (0);
-    else
-        return (*s1 < *s2 ? -1 : 1);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
 }
 
 /**
@@ -51,13 +49,10 @@ int _strcmp(char *s1, char *s2)
  */
 char *starts_with(const char *haystack, const char *needle)
 {
-    while (*needle)
-    {
-        if (*needle++ != *haystack++)
-            return (NULL);
-    }
-
-    return ((char *)haystack);
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
 }
 
 /**
@@ -69,15 +64,12 @@ char *starts_with(const char *haystack, const char *needle)
  */
 char *_strcat(char *dest, char *src)
 {
-    char *ret = dest;
+	char *ret = dest;
 
-    while (*dest)
-        dest++;
-
-    while (*src)
-        *dest++ = *src++;
-
-    *dest = *src;
-
-    return (ret);
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
