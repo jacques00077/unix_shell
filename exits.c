98b1b25b6b6772cd2dab3768f521a0cda9831e28
jacques00077 <jacquesappiagyei@gmail.com>
2023-09-20 00:41:52 -0700

    update

--- exits.c
+++ exits.c
@@ -10,25 +10,25 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i, j;
-    char *s = dest;
+	int i, j;
+	char *s = dest;
 
-    i = 0;
-    while (src[i] != '\0' && i < n - 1)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    if (i < n)
-    {
-        j = i;
-        while (j < n)
-        {
-            dest[j] = '\0';
-            j++;
-        }
-    }
-    return (s);
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
@@ -41,22 +41,22 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
-    char *s = dest;
+	int i, j;
+	char *s = dest;
 
-    i = 0;
-    j = 0;
-    while (dest[i] != '\0')
-        i++;
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    if (j < n)
-        dest[i] = '\0';
-    return (s);
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
 
 /**
@@ -64,15 +64,15 @@ char *_strncat(char *dest, char *src, int n)
  * @s: The string to be parsed.
  * @c: The character to search for.
  *
- * Return: A pointer to the first occurrence of 'c' in 's' or NULL if not found.
+ * Return: A pointer to the first occurrence of 'c' in 's' or
+ * NULL if not found.
  */
 char *_strchr(char *s, char c)
 {
-    do
-    {
-        if (*s == c)
-            return (s);
-    } while (*s++ != '\0');
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
 
-    return (NULL);
+	return (NULL);
 }
