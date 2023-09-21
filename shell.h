b95896c09e87bc2179f65cbef297393be97edbed
jacques00077 <jacquesappiagyei@gmail.com>
2023-09-21 01:30:54 -0700

    changes

--- shell.h
+++ shell.h
@@ -31,14 +31,15 @@
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
+/* Define history file settings. */
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
 
+/* External environment variable. */
 extern char **environ;
 
-
 /**
- * Define a singly linked list structure for strings.
+ * struct liststr - Define a singly linked list structure for strings.
  * @num: Number associated with the string.
  * @str: The string itself.
  * @next: Pointer to the next node in the list.
@@ -51,7 +52,7 @@ typedef struct liststr
 } list_t;
 
 /**
- * Define a structure for passing information to functions,
+ * struct passinfo - Define a structure for passing information to functions,
  * ensuring a uniform prototype
  * for function pointer structs.
  * @arg: String generated from getline containing arguments.
@@ -68,12 +69,11 @@ typedef struct liststr
  * @environ: Custom modified copy of environ from LL env.
  * @env_changed: Flag indicating if environ was changed.
  * @status: Return status of the last exec'd command.
- * @cmd_buf: Address of pointer to cmd ; chain buffer, for memory management.
+ * @cmd_buf: Pointer to cmd ; chain buffer, for management of memory.
  * @cmd_buf_type: CMD_type ||, &&, ;.
  * @readfd: File descriptor from which to read line input.
  * @histcount: History line number count.
  */
-
 typedef struct passinfo
 {
 	char *arg;
@@ -91,7 +91,7 @@ typedef struct passinfo
 	int env_changed;
 	int status;
 
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	char **cmd_buf; /* Address of pointer to cmd ; chain buffer, for memory mgt */
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
@@ -102,7 +102,7 @@ typedef struct passinfo
 		0, 0, 0}
 
 /**
- * Define a structure for built-in commands.
+ * struct builtin - Define a structure for built-in commands.
  * @type: The built-in command flag.
  * @func: The associated function.
  */
@@ -112,6 +112,126 @@ typedef struct builtin
 	int (*func)(info_t *);
 } builtin_table;
 
-/* Function declarations and more code... */
+
+/* Functions in chjay_shloop.c */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* Functions in chjay_parser.c */
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/* Functions in loophsh.c */
+int loophsh(char **);
+
+/* Functions in chjay_errors.c */
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+
+/* Functions in chjay_string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* Functions in chjay_string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/*Functions in chjay_exits.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/* Functions in chjay_tokenizer.c */
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+
+/* Functions in chjay_realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* Functions in chjay_memory.c */
+int bfree(void **);
+
+/* Functions in chjay_atoi.c */
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+/* Functions in chjay_errors1.c */
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+
+/* Functions in chjay_builtin.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* Functions in chjay_builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+/* Functions in chjay_getline.c */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+
+/* Functions in chjay_getinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+
+/* Functions in chjay_environ.c */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/* Functions in chjay_getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/* Functions in chjay_history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/* Functions in chjay_lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* Functions in chjay_lists1.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+/* Functions in chjay_vars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
 
 #endif
